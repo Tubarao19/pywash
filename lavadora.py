e6d72dc0660cf309f019c46befcdffe5a7269aa2
Tubarao19 <miguelmonsalvo19@gmail.com>
2024-12-19 10:47:34 -0500

    correcciones tiempo, total a pagar

--- lavadora.py
+++ lavadora.py
@@ -1,7 +1,9 @@
+'''para la siguiente realizar una interfaz para la misma en la libreria tkinter'''
+
 #proyecto de medicion de tiempo de uso de lavadoras teniendo en cuenta
 #que cada hora cuesta $3000 y sabiendo que una vez se llegue a las 
 #4h se le reduce en $1000 el precio total
-#2- para la siguiente realizar una interfaz para la misma en la libreria tkinter
+
 
 #libreria de tiempos
 from os import system 
@@ -18,17 +20,17 @@ delivery_time = datetime.strptime(delivery_time, "%I:%M %p")
 time_receipt = datetime.strptime(time_receipt, "%I:%M %p")
 usage_time = time_receipt - delivery_time
 #conversor de horas a entero
-usage_time = int(usage_time.total_seconds()/60/60)#/60(1h=60min)/60(1min=60seg)
+usage_time = float(usage_time.total_seconds()/60/60)#/60(1h=60min)/60(1min=60seg)
 #print(usage_time)
 #condicionales
 #buscar solucion cuando es am o cuando es pm (posiblemente deba recorrerse con ciclo for)
-if(usage_time > 4):
+if(usage_time < 4):
     price_total = usage_time * price_hour
 else:
     price_total = (usage_time * price_hour) - 1000
 
 #variable de resultado resta
 
-print(f'Tiempo de uso:\n {usage_time}h\n Total a pagar:\n ${price_total:,}')
+print(f'Tiempo de uso:\n {usage_time:.2f}h\n Total a pagar:\n ${price_total:,}')
 
 #variable de resultado en costo
